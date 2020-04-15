99105b3328e689fc9888b0d1249bf699cd3f7e07
avanboven <55521551+avanboven@users.noreply.github.com>
2020-04-15 00:46:48 -0600

    Add files via upload
    Subtraction Game updated for R2, beginning of student report

--- SubtractionGame.js
+++ SubtractionGame.js
@@ -1,14 +1,21 @@
-
  var num1;
  var num2;
  var answerTxt;
  var answerNum;
  var problemsSoFar;
  var myMap;
- var userAnswers = [];
+ var userAnswersWord;
+ var userAnswersNum;
+ var numCorrect = 0;
+ var numIncorrect = 0;
+ var percentCorrect;
+ var percentIncorrect;
+ //var allProblems = [];
 
 
 function createNumbers(){
+	userAnswersWord = [];
+	userAnswersNum = [];
 	 num1 = Math.round(Math.random()*10);
 	 num2 = Math.round(Math.random()*10);
 	if (num1 > num2){
@@ -19,6 +26,7 @@ function createNumbers(){
 		 answerTxt = num2 + " - " + num1 + " = ";
 		answerNum = num2 - num1;
 	}
+
 	
 	//cument.getElementById("num1").innerHTML = num1;
 	//cument.getElementById("num2").innerHTML = num2;
@@ -34,15 +42,19 @@ function checkAnswer(){
 	var n = document.getElementById('myInput').value;
 	var n = parseInt(n);
 	var result2 = "That is correct";
-	var result1 = "That is not correct";
+	var result1 = "That is not correct. Correct answer: " + answerNum;
+	
 	if(n == answerNum){
 		document.getElementById("res2").innerHTML = result2;
-		fillUserAnswers("correct");
+		fillUserAnswers("correct",n);
+		numCorrect++;
 	} else {
 		document.getElementById("res2").innerHTML = result1;
-		fillUserAnswers("incorrect");
+		fillUserAnswers("incorrect",n);
+		numIncorrect++;
 	}
 	document.getElementById("res2").style.visibility="visible";
+	
 }
 			
 	
@@ -52,6 +64,7 @@ function createMap(){
 	problemsSoFar = 0;
 	myMap = new Map();
 	for (i = 0; i<=10; i++){
+		//allProblems[i] = answerTxt;
 		myMap.set(answerTxt,answerNum);
 		createNumbers();
 	}
@@ -59,20 +72,48 @@ function createMap(){
 }
 
 function newProblem(){
+	document.getElementById("soFar").innerHTML = problemsSoFar+2;
+	document.getElementById("soFar").style.visibility="visible";
 	problemsSoFar++;
-	if (problemsSoFar >10){
-		var goneTooFar = "You have practiced enough problems.";
+	if (problemsSoFar >9){
+		
+		var goneTooFar = "you have done enough problems.";
 		document.write(goneTooFar);
+		//break;		
 	}
-	answerTxt = Array.from(myMap.keys())[problemsSoFar];
-	document.getElementById("problem")  = answerTxt;
-	answerNum= myMap.get(answerTxt);
+	else{
+		answerTxt = Array.from(myMap.keys())[problemsSoFar];
+		var element = document.getElementById("problem");
+		element.innerHTML  = answerTxt;
+		answerNum= myMap.get(answerTxt);
+		generateReport();
+	}
 	
 	//answerTxt = problem;
 	//answerNum = myMap.get(problem);
 }
 
-function fillUserAnswers(input){
-userAnswers[problemsSoFar] = input;
+function fillUserAnswers(wordinput, numinput){
+	userAnswersWord[problemsSoFar] = wordinput;
+	userAnswersNum[problemsSoFar] = numinput;
 }
+
+function generateReport(){
+	var userAnswersWordString = "";
+	var userAnswersNumString = "";
+	var allProblemsString = "";
+	for (i=0; i<=9; i++){
+		userAnswersWordString += userAnswersWord[i] + ".";
+		userAnswersNumString += userAnswersNum[i] + ".";
+		allProblemsString += Array.from(myMap.keys())[i] + ".";
+	}
+	userAnswersWordString += userAnswersWord[10];
+	userAnswersNumString += userAnswersNum[10];
+	allProblemsString += allProblems[10];
+	localStorage.setItem('userResponse',userAnswersWordString);
+	localStorage.setItem('userNumbers',userAnswersNumString);
+	localStorage.setItem('allProblems',allProblemsString);
+
+}
+
 
